51d7795a598ca022a41b61737848c3f44ef3b4c9
Cedric Kesssler <c.kessler@tu-berlin.de>
2025-06-23 16:49:20 +0200

    import os error fix

--- src/connection.py
+++ src/connection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 from typing import Union
 from PySide6.QtWidgets import QWidget  # pylint: disable=no-name-in-module
 from PySide6.QtWidgets import (  # pylint: disable=no-name-in-module
